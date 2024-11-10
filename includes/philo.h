da5bd4e0ea21cb77ffcd1b9963b487c482a19291
Bruno <ambsouza@gmail.com>
2024-11-10 02:39:57 -0300

    initial data created

--- includes/philo.h
+++ includes/philo.h
@@ -2,28 +2,28 @@
 # define PHILO_H
 
 # include <stdio.h>
-# include <pthread.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include "philo_data.h"
 
-typedef struct	s_philo
-{
-	pthread_t	philo;
-	int		philo_id;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		num_of_lunch;
-	struct s_philo	*next;
-}	t_philo;
+//data
+t_table	*populate_table(char **argv);
+void	populate_philo(t_table *table, t_philo *philo);
+void	link_spoon(t_philo philo, t_spoon *spoon, int i);
 
 //utils
-int		ft_atoi(const char *nptr);
+long	ft_atol(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
-void	ft_bzero(void *s, size_t n);
 int		ft_strcmp(const char *str, const char *cmp);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	ft_put_error(char	*error);
 
 //debug
-void	print_list(t_philo *list);
+void	print_list(t_table *list);
+
+//routine
+
+//mutex
+void	mutex_handle(mutex_t *mutex, m_code code);
 
 #endif
